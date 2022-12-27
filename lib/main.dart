9e8998ee190cd3f8793a0cc879348545a5c7ee0e
Kapareliotis Elias (Καπαρελιώτης Ηλίας) <helpis@tutamail.com>
2022-12-27 14:57:11 +0200

    feat: Add and configure accessibility tools package.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:accessibility_tools/accessibility_tools.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,6 +26,10 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      builder: (
+      	final BuildContext context,
+      	final Widget? child,
+     	) => AccessibilityTools(checkFontOverflows: true, child: child),
     );
   }
 }
